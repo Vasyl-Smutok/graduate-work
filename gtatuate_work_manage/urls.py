7e458ec369f13485ee55529c97f13f587d21cdfc
Vasyl Smutok <23.smutok@gmail.com>
2022-08-25 23:21:51 +0300

    Created models

--- gtatuate_work_manage/urls.py
+++ gtatuate_work_manage/urls.py
@@ -1,4 +1,4 @@
-"""web_site URL Configuration
+"""gtatuate_work_manage URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
